cfb1ff15a355b1a93889ee174b9390cd582f2e10
Shamit Soni <shamit.soni4@gmail.com>
2025-08-05 18:18:24 -0400

    Test client-server connection
    Connect the client to a test backend API to retrieve and display sample questions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <div>content</div>;
+  const [array, setArray] = useState([]);
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:5000/api");
+    setArray(response.data.questions);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <>
+      {array.map((data, index) => {
+        return (
+          <div key={index}>
+            <p>Q: {data.question}</p>
+            <p>A: {data.answer}</p>
+            <br />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
+import express from "express";
+import cors from "cors";
+import questionData from "./sample_questions.json" assert { type: "json" };
 
+const app = express();
+const port = 5000;
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+  res.json(questionData);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
