fa886d2e36e21caa824d234ff7cd77cdb21bec83
Shamit Soni <shamit.soni4@gmail.com>
2025-08-18 20:20:32 -0400

    Revert "feat: Sign-Up"
    This reverts commit a835533806884b3254cb038e2d0d24b954f46aac.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Quiz from "./components/Quiz";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/quiz" element={<Quiz />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/sign-up" element={<SignUp />}></Route>
     </Routes>
   );
 }
--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -15,10 +15,6 @@ function Quiz() {
 
   let curr = array[currentIndex];
 
-  useEffect(() => {
-    document.title = "Trivia App";
-  }, []);
-
   const nextCard = () => {
     setCurrentIndex((prevIndex) => {
       const next = prevIndex + 1 >= array.length ? 0 : prevIndex + 1;
