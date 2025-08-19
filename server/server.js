e2344d7c20029baa94aa83836436dff3a09ade9f
Shamit Soni <shamit.soni4@gmail.com>
2025-08-18 20:59:51 -0400

    Connect Sign-Up to Postgres
    Store account registration details in Postgres DB for future verification.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 import express from "express";
 import cors from "cors";
 import axios from "axios";
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = 5000;
 const corsOptions = {
   origin: ["http://localhost:5173"],
 };
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -26,7 +37,16 @@ app.get("/api/questions", async (req, res) => {
 
 app.post("/api/sign-up", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ message: `User: ${username} registered!` });
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (username, password) VALUES ($1, $2)",
+      [username, password]
+    );
+    res.json({ message: "User has been registered!" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to register user." });
+  }
 });
 
 app.listen(port, () => {
