beceaf509340d4000ce593a6679b90aeca81c6aa
Shamit Soni <shamit.soni4@gmail.com>
2025-08-28 11:53:55 -0400

    fix: HTML table semantics in CompletionTable
    - Add <thead> and <tbody> to CompletionTable.jsx to prevent React table
      structure warnings in console

--- client/src/components/Quiz/CompletionTable.jsx
+++ client/src/components/Quiz/CompletionTable.jsx
@@ -1,30 +1,33 @@
-function CompletionTable(props) {
+function CompletionTable({ questions, answered }) {
   return (
     <>
       <div className="progress-container">
         <div
           className="progress-bar"
-          style={{ width: `${Object.keys(props.answered).length * 10}%` }}
+          style={{ width: `${Object.keys(answered).length * 10}%` }}
         ></div>
       </div>
       <table>
-        <tr>
-          <th style={{ paddingRight: "30px" }}>Question</th>
-          <th>Status</th>
-        </tr>
-
-        {props.questions.map((ques, index) => (
+        <thead>
           <tr>
-            <td>#{ques.id}</td>
-            <td style={{ color: "green" }}>
-              {props.answered[index]
-                ? props.answered[index] === ques.answer
-                  ? "✅"
-                  : "❌"
-                : "-"}
-            </td>
+            <th style={{ paddingRight: "30px" }}>Question</th>
+            <th>Status</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {questions.map((ques, index) => (
+            <tr key={index}>
+              <td>#{ques.id}</td>
+              <td style={{ color: "green" }}>
+                {answered[index]
+                  ? answered[index] === ques.answer
+                    ? "✅"
+                    : "❌"
+                  : "-"}
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );
