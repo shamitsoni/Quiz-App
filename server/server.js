c1d0475f2ea8bc856ad51682c3610bd13779a1e1
Shamit Soni <shamit.soni4@gmail.com>
2025-09-05 17:46:27 -0400

    feat: Add env variable support for server URL in Docker setup
    - Add VITE_SERVER_URL build arg to frontend service
    - Update Dockerfile to make env variable available at build time
    - Replace hardcoded API URLs with configurable environment variable

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const port = 5000;
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:3000"],
 };
 
 const pool = new Pool({
