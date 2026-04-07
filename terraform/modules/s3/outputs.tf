ba45dfe5b2901d16436cd6b95677e8de06da55b5
Shamit Soni <shamit.soni4@gmail.com>
2026-04-07 12:50:56 -0400

    Terraform: Edit S3 module outputs
    Add bucket_name and bucket_regional_domain_name to s3 module outputs
    for future use in cloudfront module

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -6,4 +6,14 @@ output "bucket_id" {
 output "bucket_arn" {
   value       = aws_s3_bucket.frontend-bucket.arn
   description = "The ARN of the S3 bucket"
-}   
+}
+
+output "bucket_name" {
+  value       = aws_s3_bucket.frontend-bucket.bucket
+  description = "The name of the S3 bucket"
+}
+
+output "bucket_regional_domain_name" {
+  value       = aws_s3_bucket.frontend-bucket.bucket_domain_name
+  description = "The regional domain name of the S3 bucket"
+}
