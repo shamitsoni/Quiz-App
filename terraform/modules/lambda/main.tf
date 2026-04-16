79721f221a6a451b1cb4523dd430e9dacdb18d49
Shamit Soni <shamit.soni4@gmail.com>
2026-04-16 10:45:02 -0400

    Terraform: Initial lambda skeleton

---
+++ terraform/modules/lambda/main.tf
@@ -0,0 +1,37 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_execution" {
+  name               = "lambda_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_lambda_function" "example" {
+  function_name = "quiz-backend-lambda"
+  role          = aws_iam_role.lambda_execution.arn
+
+  handler = "index.handler"
+  runtime = "nodejs22.x"
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+      LOG_LEVEL   = "info"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Application = "example"
+  }
+}
